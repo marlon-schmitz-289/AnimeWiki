c93e9c12d88d3cad17a58acb94fb42dcda691552
Marlon Schmitz <marlonschmitz289@gmail.com>
2023-03-10 12:07:41 +0100

    Initial Commit

--- DragonBallWiki/ViewModel/CharacterEditViewModel.cs
+++ DragonBallWiki/ViewModel/CharacterEditViewModel.cs
@@ -1,15 +1,43 @@
-﻿using System.Windows.Input;
+﻿using DragonBallWiki.Commands;
+using DragonBallWiki.Models;
+using DragonBallWiki.Stores;
+using DragonBallWiki.Views.Pages;
+using System.Windows.Input;
 
 namespace DragonBallWiki.ViewModel
 {
     public class CharacterEditViewModel : BaseModel
     {
-        public ICommand CloseCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand BackCommand { get; set; }
+
+
+        private Character currCharacter;
+        public Character CurrCharacter
+        {
+            get => currCharacter;
+            set
+            {
+                currCharacter = value;
+                OnPropertyChanged(nameof(CurrCharacter));
+            }
+        }
 
 
         public CharacterEditViewModel ()
         {
-            // Penis
+            CurrCharacter = new();
+
+            AddCommand = new BaseCommand((o) =>
+            {
+                // save CurrCharacter to database
+            }, (o) => CurrCharacter is not null);
+
+            BackCommand = new BaseCommand((o) =>
+            {
+                CurrCharacter = new();
+                CurrPageStore.SetCurr(new WikiPage());
+            });
         }
     }
 }
